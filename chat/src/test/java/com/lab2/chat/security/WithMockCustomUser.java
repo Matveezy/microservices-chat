21612f248a539bac11235fd9f32469897a0adb43
Matvey Chukhno <mr.chucno@yandex.ru>
2024-02-06 12:15:38 +0300

    feat: Chat Service tests
    -Add liquibase migrations for application and test
    -Add r2dbc test containers
    -Add tests

---
+++ chat/src/test/java/com/lab2/chat/security/WithMockCustomUser.java
@@ -0,0 +1,18 @@
+package com.lab2.chat.security;
+
+import com.lab2.chat.entity.Role;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+
+    String username() default "user";
+
+    String pass() default "pass";
+
+    Role authority() default Role.ROLE_USER;
+}
